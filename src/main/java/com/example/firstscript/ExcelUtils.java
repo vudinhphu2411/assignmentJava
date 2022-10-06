8b55335c77107647a80168ab6f127eda8cf12a1f
SETH <vudinhphu933@gmail.com>
2022-10-06 10:58:51 +0700

    update

--- src/main/java/com/example/firstscript/ExcelUtils.java
+++ src/main/java/com/example/firstscript/ExcelUtils.java
@@ -1,93 +1,10 @@
 package com.example.firstscript;
 
-// package com.example.firstscript;
-
-// import java.io.FileInputStream;
-// import java.io.IOException;
-// import java.io.InputStream;
-
-// import org.apache.poi.hssf.usermodel.HSSFSheet;
-// import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-// import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-// import org.apache.poi.ss.usermodel.DataFormatter;
-
-// public class ExcelUtils {
-
-// static HSSFWorkbook workbook;
-// static HSSFSheet sheet;
-
-// public ExcelUtils(String excelPath, String sheetName) {
-// try {
-// InputStream file = new FileInputStream(excelPath);
-
-// workbook = new HSSFWorkbook(new POIFSFileSystem(file));
-// sheet = workbook.getSheet(sheetName);
-// } catch (Exception e) {
-// System.out.println(e.getCause());
-// System.out.println(e.getMessage());
-// e.printStackTrace();
-// }
-// }
-
-// public static void getCellData(int rowNum, int collNum) throws IOException {
-// DataFormatter formatter = new DataFormatter();
-// Object value =
-// formatter.formatCellValue(sheet.getRow(rowNum).getCell(collNum));
-// System.out.println(value);
-// }
-
-// public void getRowCount() {
-// int rowCount = sheet.getPhysicalNumberOfRows();
-// System.out.println("No of row: " + rowCount);
-// }
-// }
-
-// ----------------------------------------------------------
-// import java.io.FileInputStream;
-// import java.io.IOException;
-// import java.util.Iterator;
-
-// import org.apache.poi.EncryptedDocumentException;
-// import org.apache.poi.ss.usermodel.Cell;
-// import org.apache.poi.ss.usermodel.Row;
-// import org.apache.poi.xssf.usermodel.XSSFSheet;
-// import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-// public class ExcelUtils {
-// private static XSSFWorkbook workbook;
-// int cellIndex;
-// String description;
-
-// @Test
-// public void excel() throws EncryptedDocumentException, IOException {
-// // get the excel sheet file location
-// FileInputStream fis = new
-// FileInputStream("C:/Users/Administrator/Desktop/assignment2.xlsx");
-// workbook = new XSSFWorkbook(fis);
-// // XSSFSheet sheet = workbook.getSheetAt(0);
-// XSSFSheet sheet = workbook.getSheet("Sheet1");
-// // Duyệt các ptu từ đầu đến cuối của 1 collection
-// Iterator<Row> iterator = sheet.rowIterator();
-// cellIndex = 0;
-// description = null;
-// while (iterator.hasNext()) {
-// Row row = iterator.next();
-
-// for (int i = 0; i < row.getPhysicalNumberOfCells(); i++) {
-// Cell cell = row.getCell(i);
-// // print the cell value
-// System.out.println(i + " - " + cell);
-// }
-// }
-// }
-// }
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
--- src/main/java/com/example/firstscript/Mainclass.java
+++ src/main/java/com/example/firstscript/Mainclass.java
@@ -11,10 +11,11 @@ public class Mainclass {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String args[]) throws Exception {
-        // System.out.println("1: Exel File");
-        // Thread.sleep(2000);
-        // ExcelUtils excel = new ExcelUtils();
-        // excel.excel();
+        System.out.println("1: Exel File");
+        Thread.sleep(2000);
+        ExcelUtils excel = new ExcelUtils();
+        excel.excel();
+        System.out.println("\n");
 
         Thread.sleep(3000);
         System.out.println("2: XML File");
@@ -24,29 +25,33 @@ public class Mainclass {
         System.out.println("Waitting.......");
         Thread.sleep(3000);
         xml.update();
+        System.out.println("\n");
+        Thread.sleep(3000);
 
-        // System.out.println("3: JSON File");
-        // System.out.println("Read JsonFile....................");
-        // JSONObject json = Json.read();
+        System.out.println("3: JSON File");
+        System.out.println("Read JsonFile....................");
+        JSONObject json = Json.read();
 
-        // // Nhap ten Field muon update
-        // System.out.print("Enter field name: ");
-        // String fieldName = scanner.nextLine();
+        // Nhap ten Field muon update
+        System.out.print("Enter field name: ");
+        String fieldName = scanner.nextLine();
 
-        // json = Json.updateJson(json, fieldName);
+        json = Json.updateJson(json, fieldName);
 
-        // Json.write(json);
-        // Thread.sleep(2000);
-        // System.out.printf("\n");
+        Json.write(json);
+        Thread.sleep(2000);
+        System.out.printf("\n");
 
-        // System.out.println("After Update and save JsonFile....................");
-        // System.out.println(Json.read());
+        System.out.println("After Update and save JsonFile....................");
+        System.out.println(Json.read());
+        System.out.println("\n");
+        Thread.sleep(3000);
 
-        // System.out.println("4: Text File");
-        // Thread.sleep(2000);
-        // Assignment1 ass1 = new Assignment1();
-        // ass1.txt();
-        // System.out.println(" ");
+        System.out.println("4: Text File");
+        Thread.sleep(2000);
+        Assignment1 ass1 = new Assignment1();
+        ass1.txt();
+        System.out.println(" ");
 
     }
 }
